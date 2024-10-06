ea7f2b5e8b2d18633fe8e0c3a3c5c1996dd2fff8
Gabriel Ferreira Umbelino <gabriel.f.umbelino@gmail.com>
2024-10-06 19:16:46 -0300

    update customer controller

--- src/domain/adapters/driving/customerController.ts
+++ src/domain/adapters/driving/customerController.ts
@@ -1,23 +1,71 @@
-import { Body, Controller, Get, Logger, Post } from '@nestjs/common';
+import { FilterCustomerDto } from './../../ports/model/customer';
+import {
+  Body,
+  Controller,
+  Get,
+  Logger,
+  Post,
+  Query,
+  Req,
+} from '@nestjs/common';
 import { CustomerService } from '../../ports/inboundPorts/customerService';
-import { CustomerCommand } from '../model/customerCommand';
+import { Customer, CustomerDto } from '../../ports/model/customer';
+import { ApiQuery } from '@nestjs/swagger';
 
 @Controller('customer')
 export class CustomerController {
   private readonly logger = new Logger(CustomerController.name);
-
   constructor(private customerService: CustomerService) {}
 
   @Get()
-  findAll(): any[] {
+  findAll(): Promise<Customer[]> {
     return this.customerService.findAll();
   }
 
+  @ApiQuery({
+    name: "id",
+    type: String,
+    required: false
+  })
+  @ApiQuery({
+    name: "name",
+    type: String,
+    required: false
+  })
+  @ApiQuery({
+    name: "document",
+    type: String,
+    required: false
+  })
+  @ApiQuery({
+    name: "phoneNumber",
+    type: String,
+    required: false
+  })
+
+  @Get(':params')
+  find(
+    @Query('id') id?: string,
+    @Query('name') name?: string,
+    @Query('document') document?: string,
+    @Query('phoneNumber') phoneNumber?: string,
+  ): Promise<Customer[]> {
+
+    const filterCustomerDto: FilterCustomerDto = {
+      id,
+      name,
+      document,
+      phoneNumber,
+    };
+
+    return this.customerService.find(filterCustomerDto);
+  }
+
   @Post()
-  create(@Body() customerCommand: CustomerCommand): any {
-    const customer = this.customerService.create(customerCommand.name);
-    this.logger.debug(customerCommand);
+  async create(@Body() customerDto: CustomerDto): Promise<Customer> {
+    const customer = await this.customerService.create(customerDto);
+    this.logger.debug(customerDto);
     this.logger.debug({ customer });
-    return { ...customer };
+    return customer;
   }
 }
