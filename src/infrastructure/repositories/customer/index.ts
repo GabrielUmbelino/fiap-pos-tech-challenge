fb6b38ace38bc1241b792feb817dab9526091332
Gabriel Ferreira Umbelino <gabriel.f.umbelino@gmail.com>
2024-10-17 11:01:35 -0300

    muda campos no banco para minusculo

--- src/infrastructure/repositories/customer/index.ts
+++ src/infrastructure/repositories/customer/index.ts
@@ -1,5 +1,4 @@
 export * from './customer.entity';
-export * from './guest.entity';
 export * from './customerInDbRepository';
 export * from './mockCustomerRepository';
 export * from './customerInMemoryRepository';
